737034fb9d05baadfe02029f40fb9dcc3813cddb
PUMOVETZ <batsiev2006@gmail.com>
2024-07-21 22:28:17 +0300

    fix captcha leaker

--- Captcha.py
+++ Captcha.py
@@ -1,11 +1,12 @@
 import json
 import asyncio
+import uuid
 from concurrent.futures import ThreadPoolExecutor
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as waiter
 
 # json
 with open('config.json', 'r', encoding='utf-8') as f:
@@ -21,6 +22,7 @@ with open('config.json', 'r', encoding='utf-8') as f:
 status = 0
 captcha = 0
 # 0 - init 1 - open login 2 - select type 3 change type 4 - success
+
 
 # Settings
 chrome_options = Options()
@@ -43,16 +45,14 @@ executor = ThreadPoolExecutor(max_workers=2)
 async def wait_for_captcha():
     loop = asyncio.get_event_loop()
     try:
-        element = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                             EC.presence_of_element_located((By.XPATH, '//*[@id="tiktok-verify-ele"]')))
+        await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
+                                   waiter.presence_of_element_located(
+                                       (By.XPATH, '//*[@id="captcha_container"]/div')))
         print("Капча найдена")
 
         await asyncio.sleep(delay)
 
-        # Делаем скриншот страницы
-        screenshot_path = "captcha_screenshot.png"
-        driver.save_screenshot(screenshot_path)
-        print(f"Скриншот страницы сохранен в файл {screenshot_path}")
+        driver.save_screenshot(f"{uuid.uuid4()}.png")
 
         return True
     except Exception as e:
@@ -64,81 +64,89 @@ async def login():
     loop = asyncio.get_event_loop()
     try:
         await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                   EC.presence_of_element_located(
+                                   waiter.presence_of_element_located(
                                        (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[1]/input')))
-        print("Форма логина найдена")
 
         login_field = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                 EC.presence_of_element_located(
+                                                 waiter.presence_of_element_located(
                                                      (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/div[1]/input')))
         login_field.send_keys(login_data)
-        print("Логин заполнен")
         await asyncio.sleep(delay)
         password_field = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                    EC.presence_of_element_located((By.XPATH,
-                                                                                    '//*[@id="loginContainer"]/div[2]/form/div[2]/div/input')))
+                                                    waiter.presence_of_element_located((By.XPATH,
+                                                                                        '//*[@id="loginContainer"]/div[2]/form/div[2]/div/input')))
         password_field.send_keys(password_data)
-        print("Пароль заполнен")
         await asyncio.sleep(delay)
 
         login_button = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                  EC.element_to_be_clickable(
+                                                  waiter.element_to_be_clickable(
                                                       (By.XPATH, '//*[@id="loginContainer"]/div[2]/form/button')))
         login_button.click()
-        print("Кнопка входа нажата")
     except Exception as e:
         print(f"Произошла ошибка при заполнении формы логина: {e}")
 
 
-async def wait_for_button():
+async def clicker():
+    global status
     loop = asyncio.get_event_loop()
     while True:
-        if await loop.run_in_executor(executor, lambda: driver.find_elements(By.XPATH, '//*[@id="tiktok-verify-ele"]')):
-            screenshot_path = "captcha_screenshot.png"
-            driver.save_screenshot(screenshot_path)
-            print(f"Скриншот страницы сохранен в файл {screenshot_path}")
+        match status:
+            case 0:
+                try:
+                    login_button = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
+                                                              waiter.element_to_be_clickable(
+                                                                  (By.XPATH, '//*[@id="header-login-button"]')))
+                    login_button.click()
+                    status = 1
+                except Exception as e:
+                    print(f"Can't press login button with error {e}")
+                    continue
+            case 1:
+                try:
+                    await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
+                                               waiter.presence_of_element_located(
+                                                   (By.XPATH, '//*[@id="login-modal"]/div[1]')))
 
-        try:
-            login_button = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                      EC.element_to_be_clickable(
-                                                          (By.XPATH, '//*[@id="header-login-button"]')))
-            login_button.click()
-            print("Кнопка нажата")
+                    await asyncio.sleep(delay)
 
-            await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                       EC.presence_of_element_located((By.XPATH, '//*[@id="login-modal"]/div[1]')))
-            print("Элемент login-modal найден")
+                    change_login_type = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
+                                                                   waiter.element_to_be_clickable(
+                                                                       (By.XPATH,
+                                                                        '//*[@id="loginContainer"]/div/div/div/div[2]')))
+                    change_login_type.click()
+                    status = 2
+                except Exception as e:
+                    print(f"Can't select login with error {e}")
+                    continue
+            case 2:
+                try:
+                    change_login_type = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
+                                                                   waiter.element_to_be_clickable(
+                                                                       (By.XPATH,
+                                                                        '//*[@id="loginContainer"]/div/form/div[1]/a')))
+                    change_login_type.click()
+                    status = 3
+                except Exception as e:
+                    print(f"Can't change login type with error {e}")
+                    continue
 
-            await asyncio.sleep(delay)
+            case 3:
+                try:
+                    await login()
+                    status = 4
+                except Exception as e:
+                    print(f"Can't login with error {e}")
+            case 4:
 
-            change_login_type = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                           EC.element_to_be_clickable(
-                                                               (By.XPATH,
-                                                                '//*[@id="loginContainer"]/div/div/div/div[2]')))
-            change_login_type.click()
-            print("Вторая кнопка нажата")
-
-            change_login_type = await loop.run_in_executor(executor, WebDriverWait(driver, timeout).until,
-                                                           EC.element_to_be_clickable(
-                                                               (By.XPATH,
-                                                                '//*[@id="loginContainer"]/div/form/div[1]/a')))
-            change_login_type.click()
-
-            print("Третья кнопка нажата")
-
-            await login()
-
-            await asyncio.sleep(50)
-            print("here")
-            await asyncio.sleep(timeout)
-            return
-        except Exception as e:
-            print(f"Произошла ошибка при поиске или нажатии кнопки: {e}")
+                await asyncio.sleep(50)
+                print("here")
+                await asyncio.sleep(timeout)
+                return
 
 
 async def main():
     captcha_task = asyncio.create_task(wait_for_captcha())
-    button_task = asyncio.create_task(wait_for_button())
+    button_task = asyncio.create_task(clicker())
 
     await captcha_task
     await button_task
