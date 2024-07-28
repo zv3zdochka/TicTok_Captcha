00942c8f43454c60c4cb31175254f1a311fd931e
PUMOVETZ <batsiev2006@gmail.com>
2024-07-28 14:10:39 +0300

    add model

--- Captcha.py
+++ Captcha.py
@@ -1,5 +1,6 @@
 import json
 import asyncio
+import random
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from selenium import webdriver
@@ -15,12 +16,13 @@ class Bot():
         self.id = str(id)
         self.load_config()
         self.status = 0  # 0 - not done 1 - in process 2 - done
-        self.captcha = 0  # 0 - init 1 - open login 2 - select type 3 change type 4 - success
+        self.captcha = 0  # 0 - init 1 - open login 2 - select type 3 change type 4 - typped 5 - successful login
         self.chrome_options = self.setup_chrome_options()
         self.driver = None
         self.executor = ThreadPoolExecutor(max_workers=3)
         self.bot_status = 1  # 0 -in process 1 - created and wait, 2 - logined and scrolling
         self.com = 'comment text'
+        self.model = 0  # 0 swapping & like & comment, 1 - enter strim 
         loop = asyncio.get_event_loop()
         if loop.is_running():
             asyncio.create_task(self.wait())
@@ -46,18 +48,19 @@ class Bot():
                 self.timeout = data['timeout']
                 self.delay = data['delay']
 
+
         except Exception as e:
             exit(f"Fail on loading file {e}")
 
     def setup_chrome_options(self) -> Options:
         options = Options()
-        #options.add_argument(f'--proxy-server={self.proxy}')
+        # options.add_argument(f'--proxy-server={self.proxy}')
         options.add_argument('--disable-infobars')
         options.add_argument('--disable-extensions')
         options.add_argument('--disable-plugins-discovery')
         options.add_argument('--disable-blink-features=AutomationControlled')
         options.add_argument("--start-maximized")
-        #options.add_argument("--headless=new")
+        # options.add_argument("--headless=new")
         return options
 
     async def wait_for_captcha(self):
@@ -67,19 +70,12 @@ class Bot():
                 await loop.run_in_executor(self.executor, WebDriverWait(self.driver, self.timeout).until,
                                            waiter.presence_of_element_located(
                                                (By.XPATH, '//*[@id="captcha_container"]/div')))
-
                 print("Капча найдена")
-
                 self.captcha = 1
-
                 await asyncio.sleep(self.delay)
-
                 self.driver.save_screenshot(f"{uuid.uuid4()}.png")
-
                 await asyncio.sleep(self.delay)
-
                 self.captcha = 2
-
                 return True
             except Exception as e:
                 print(f"Произошла ошибка при поиске капчи: {e}")
@@ -92,20 +88,12 @@ class Bot():
                 await loop.run_in_executor(self.executor, WebDriverWait(self.driver, self.timeout).until,
                                            waiter.presence_of_element_located(
                                                (By.XPATH, '//*[@id="tiktok-verify-ele"]/div')))
-
                 print("")
-
                 self.captcha = 1
-
-
                 await asyncio.sleep(self.delay)
-
                 self.driver.save_screenshot(f"{uuid.uuid4()}.png")
-
                 await asyncio.sleep(self.delay)
-
                 self.captcha = 2
-
                 return True
             except Exception as e:
                 print(f"Произошла ошибка при поиске капчи: {e}")
@@ -189,10 +177,28 @@ class Bot():
                     except Exception as e:
                         print(f"Can't login with error {e}")
                 case 4:
-                    await asyncio.sleep(50)
-                    print("Ждем команд")
-                    await asyncio.sleep(self.timeout)
+                    await loop.run_in_executor(self.executor,
+                                               WebDriverWait(self.driver, self.timeout).until,
+                                               waiter.element_to_be_clickable(
+                                                   (By.XPATH, '//*[@id="header-more-menu-icon"]')))
+                    self.status = 5
                     return
+
+    async def online(self):
+        while True:
+            match self.model:
+                case 0:
+                    t = random.randrange(2, 15)
+                    await asyncio.sleep(t)
+                    if t == random.randrange(1, 20):
+                        await self.like()
+                    if t == random.randrange(1, 100):
+                        await self.comment()
+                    await self.swap()
+                case 1:
+                    pass
+
+
 
     async def like(self):
         loop = asyncio.get_event_loop()
@@ -200,6 +206,9 @@ class Bot():
                                          waiter.presence_of_element_located((By.XPATH,
                                                                              '//*[@id="main-content-homepage_hot"]/div[1]/div[1]/div/div/div[2]/button[1]')))
         but.click()
+
+    async def swap(self):
+        pass
 
     async def comment(self):
         loop = asyncio.get_event_loop()
@@ -225,18 +234,19 @@ class Bot():
 
         captcha_task = asyncio.create_task(self.wait_for_captcha())
         button_task = asyncio.create_task(self.clicker())
-
+        online_task = asyncio.create_task(self.online())
         await captcha_task
         await button_task
 
 
 if __name__ == "__main__":
+    print()
     bot = Bot(123123)
     # bot.run()
 
+
 # //*[@id="tiktok-verify-ele"]/div box с капчей
 #
-# //*[@id="verify-bar-close"] его можно закрыть
+# //*[@id="verify-bar-close"] его можно закрыть=
 
 # guger1231
-
